6defdb343aab5691be895b0d4a1601bc1647aba0
PhuNguyen <boyhayhaha12345@gmail.com>
2026-06-01 15:50:09 +0700

    feat: hoàn thành CRUD movie và custom toast UX

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Dashboard from './pages/admin/Dashboard';
 import Home from './pages/user/Home';
 import Login from './pages/auth/Login';
 import RequireAuth from './components/RequireAuth'; 
+import ManageMovie from './pages/admin/ManageMovie';
 
 function App() {
   return (
     <BrowserRouter>
+    
       <Routes>
         {/* Tuyến đường công khai: Đăng nhập */}
         <Route path="/login" element={<Login />} />
@@ -21,6 +23,7 @@ function App() {
         {/* 2. BỌC REQUIRE_AUTH Ở ĐÂY ĐỂ BẢO VỆ TOÀN BỘ TRANG ADMIN */}
         <Route element={<RequireAuth />}>
           <Route path="/admin" element={<AdminLayout />}>
+            <Route path="movies" element={<ManageMovie />} />
             <Route path="dashboard" element={<Dashboard />} />
             {/* Nếu sau này có thêm trang quản lý phim, user... bạn cứ ném vào trong cụm này */}
           </Route>
